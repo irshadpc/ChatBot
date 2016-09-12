db7ba748638b6f4a50dae8f7bd2fc03384e7a7ac
Andrey Denisov <andreyd@hexagontech.co.il>
2016-09-12 19:57:19 +0300

    UI : add messaging view controller

---
+++ FinancialChatBot/FinancialChatBot-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FinancialChatBot-Bridging-Header.h.h
+//  FinancialChatBot
+//
+//  Created by Andrew Denisov on 9/12/16.
+//  Copyright © 2016 Andrew Denisov. All rights reserved.
+//
+
+#ifndef FinancialChatBot_Bridging_Header_h_h
+#define FinancialChatBot_Bridging_Header_h_h
+
+#import <JSQMessagesViewController/JSQMessages.h> 
+
+#endif /* FinancialChatBot_Bridging_Header_h_h */
+
---
+++ FinancialChatBot/FinancialChatBot/UI/MessagesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MessagesViewController.swift
+//  FinancialChatBot
+//
+//  Created by Andrew Denisov on 9/12/16.
+//  Copyright © 2016 Andrew Denisov. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import JSQMessagesViewController
+
+class MessagesViewController :  JSQMessagesViewController {
+    
+}
