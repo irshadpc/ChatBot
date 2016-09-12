5cbda8cfad6eb5556f5c7c044a0116ae427988a4
Andrey Denisov <andreyd@hexagontech.co.il>
2016-09-12 16:16:51 +0300

    Initial commit : create skeleton for application

---
+++ FinancialChatBot/FinancialChatBot/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FinancialChatBot
+//
+//  Created by Andrew Denisov on 9/12/16.
+//  Copyright © 2016 Andrew Denisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
