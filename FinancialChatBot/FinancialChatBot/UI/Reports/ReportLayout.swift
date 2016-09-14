772bb3c2684c673b5da013a8131ea82a6ad5f645
Andrey Denisov <adenisov@provectus-it.com>
2016-09-14 08:59:45 +0300

    UI : add skeleton for Reports

---
+++ FinancialChatBot/FinancialChatBot/UI/Reports/ReportLayout.swift
@@ -0,0 +1,9 @@
+//
+//  ReportLayout.swift
+//  FinancialChatBot
+//
+//  Created by Andrew on 9/14/16.
+//  Copyright © 2016 Andrew Denisov. All rights reserved.
+//
+
+import Foundation
---
+++ FinancialChatBot/FinancialChatBot/UI/Reports/ReportViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ReportViewController.swift
+//  FinancialChatBot
+//
+//  Created by Andrew on 9/14/16.
+//  Copyright © 2016 Andrew Denisov. All rights reserved.
+//
+
+import Foundation
